b07b8f4d3190400156de2713d67e7360fed8090e
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-10-03 19:45:15 +0300

    network module done

---
+++ terraform/network/outputs.tf
@@ -0,0 +1,11 @@
+output main_vpc {
+value = google_compute_network.vpc_network.self_link
+}
+
+output managment_subnet {
+    value = google_compute_subnetwork.managment_subnet.self_link
+}
+
+output workload_subnet {
+    value = google_compute_subnetwork.workload_subnet.self_link
+}
